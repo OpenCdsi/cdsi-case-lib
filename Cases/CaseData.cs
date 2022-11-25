8cc262de960eee2e73d70a2758d6761f91273781
Dennis Dunn <dennisdunn@users.noreply.github.com>
2022-11-25 11:01:51 -0500

    Added version property to the API.

--- Cases/CaseData.cs
+++ Cases/CaseData.cs
@@ -6,5 +6,6 @@ namespace OpenCdsi
     {
         private static readonly IReadOnlyDictionary<string, testcase> _cases = (IReadOnlyDictionary<string, testcase>)Factories.CreateTestcaseMap();
         public static IReadOnlyDictionary<string, testcase> Cases => _cases;
+        public static string ResourceVersion =>Metadata.ResourceVersion;
     }
 }
